8e857a14971f6226d55c51d6c3b4177e2950210c
akyadav4710 <163339150+akyadav4710@users.noreply.github.com>
2024-08-01 19:58:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+
+function capitalizeName() {
+    const inputField = document.getElementById('fname');
+    inputField.value = inputField.value.toUpperCase();
+}
+
+
+document.getElementById('fname').addEventListener('blur', capitalizeName);
